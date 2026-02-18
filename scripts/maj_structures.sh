ef282f466cc51488b76e7237ce43d54d1c8afa7a
Caroline <famille.nony@sfr.fr>
2026-02-18 09:25:30 +0100

    modif scripts user root

--- scripts/maj_structures.sh
+++ scripts/maj_structures.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #fichier environnement pour les variables d'environnement (pas de .env pour ne pas risquer de le commiter par erreur, et pas d'export dans le .env pour pouvoir les utiliser dans docker compose)
-source /home/ubuntu/.predictelec_env
+#source /home/ubuntu/.predictelec_env
+source /root/.predictelec_env
 
 # autorise erreur sans stopper le script : pas -e, sinon on ne récupère pas l'erreur à envoyer par mail
 set +e
@@ -12,7 +13,7 @@ LOCK_FILE="/tmp/maj_structures.lock"
 cd "$(dirname "$0")/.."
 
 PROJECT_DIR="$(pwd)"
-LOG_DIR="$PROJECT_DIR/logs"
+LOG_DIR="/opt/predictelec/shared/logs"
 LOG_FILE="$LOG_DIR/maj_structures.log"
 
 mkdir -p "$LOG_DIR"
@@ -31,7 +32,7 @@ echo "Début MAJ_STRUCTURES : $(date)" >> "$LOG_FILE"
 
 # Exécution du job 
 # ajout --no-deps pour ne pas relancer postgres
-docker compose run --rm --no-deps app MAJ_STRUCTURES >> "$LOG_FILE" 2>&1
+docker compose -f "$PROJECT_DIR/docker-compose.yml" run --rm --no-deps app MAJ_STRUCTURES >> "$LOG_FILE" 2>&1
 EXIT_CODE=$?
 
 #stop le script si erreur
